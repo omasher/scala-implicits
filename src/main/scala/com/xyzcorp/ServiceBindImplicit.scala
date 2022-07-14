a3e5a564b9de432d0cd6e8e4b52da6e349829a7e
omasher <ashermcoa@gmail.com>
2022-07-14 18:26:21 +0300

    Binding services implicitly

--- src/main/scala/com/xyzcorp/ServiceBindImplicit.scala
+++ src/main/scala/com/xyzcorp/ServiceBindImplicit.scala
@@ -1,5 +1,22 @@
 package com.xyzcorp
 
-object ServiceBindImplicit {
+object ServiceBindImplicit extends App {
 
+  import scala.concurrent._
+  import java.util.concurrent.Executors
+
+  val executor = Executors.newFixedThreadPool(4)
+  implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(executor)
+
+  val future = Future.apply {
+    println(s"Thread-name: ${Thread.currentThread().getName}")
+    Thread.sleep(3000)
+    50 + 100
+  }
+  future
+    .map(x => x * 100)
+    .foreach(a => println(a))
+
+  Thread.sleep(5000)
+  System.exit(0)
 }
