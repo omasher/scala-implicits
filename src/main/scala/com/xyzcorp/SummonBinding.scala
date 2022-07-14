67847b507431d114a350a113197e75ef99dbdbf3
omasher <ashermcoa@gmail.com>
2022-07-14 18:37:26 +0300

    Using implicitly to summon bindings

---
+++ src/main/scala/com/xyzcorp/SummonBinding.scala
@@ -0,0 +1,15 @@
+package com.xyzcorp
+
+case class IceCream(name: String)
+
+case class Scoops(num: Int, flavor: IceCream)
+
+object SummonBinding extends App {
+  implicit val flavorOfTheMonth: IceCream = IceCream("Rainbow Sherbet")
+
+  def orderIceCream(num: Int)(implicit flavorOfTheMonth: IceCream) = {
+    Scoops(num, flavorOfTheMonth)
+  }
+
+  println(orderIceCream(4))
+}
