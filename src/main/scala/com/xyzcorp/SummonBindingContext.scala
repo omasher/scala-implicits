cd31ba5738a7a4235974ecbf7dc1ca2b82f51afe
omasher <ashermcoa@gmail.com>
2022-07-14 19:00:34 +0300

    Differentiating parameterizes types

--- src/main/scala/com/xyzcorp/SummonBindingContext.scala
+++ src/main/scala/com/xyzcorp/SummonBindingContext.scala
@@ -6,10 +6,15 @@ case class Scoops(num: Int, flavor: IceCream)
 
 object SummonBindingContext extends App {
   implicit val flavorOfTheMonth: IceCream = IceCream("Rainbow Sherbet")
+  implicit val listOfStrings: List[String] = List("Foo", "Bar", "Baz")
+  implicit val listOfDouble: List[Double] = List(1.0, 2.0, 3.0)
 
   def orderIceCream(num: Int) = {
     Scoops(num, implicitly[IceCream])
   }
 
   println(orderIceCream(4))
+
+  val result = implicitly[List[Double]]
+  println(result(1))
 }
