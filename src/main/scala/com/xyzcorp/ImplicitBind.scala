7ec6443fdce028bd32befbff9c4e722b248e7376
omasher <ashermcoa@gmail.com>
2022-07-14 18:16:29 +0300

    Binding services manually

---
+++ src/main/scala/com/xyzcorp/ImplicitBind.scala
@@ -0,0 +1,22 @@
+package com.xyzcorp
+
+object ImplicitBind extends App {
+
+  import scala.concurrent._
+  import java.util.concurrent.Executors
+
+  val executor = Executors.newFixedThreadPool(4)
+  val executionContext: ExecutionContext = ExecutionContext.fromExecutor(executor)
+
+  val future = Future.apply {
+    println(s"Thread-name: ${Thread.currentThread().getName}")
+    Thread.sleep(3000)
+    50 + 100
+  }(executionContext)
+  future
+    .map(x => x * 100)(executionContext)
+    .foreach(a => println(a))(executionContext)
+
+  Thread.sleep(5000)
+  System.exit(0)
+}
