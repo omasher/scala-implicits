d354512e1bfff291eef711f91e96d91c1e51a781
omasher <ashermcoa@gmail.com>
2022-07-14 17:50:28 +0300

    Manual overrides

--- src/main/scala/com/xyzcorp/MyApp.scala
+++ src/main/scala/com/xyzcorp/MyApp.scala
@@ -3,11 +3,11 @@ package com.xyzcorp
 object MyApp {
   implicit val rate: Int = 100
 
-  def calcPayment(hours: Int)(implicit n: Int): Int = hours * n
+  def calcPayment(hours: Int)(implicit rate: Int): Int = hours * rate
 
 
   def main(args: Array[String]): Unit = {
-    println(calcPayment(30))
+    println(calcPayment(30)(110))
   }
 
 }
