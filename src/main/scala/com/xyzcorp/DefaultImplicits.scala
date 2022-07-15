a6b69da7475ad8fc529b92802e21e3e0db1a2c38
omasher <ashermcoa@gmail.com>
2022-07-15 04:39:01 +0300

    Primitive conversions

--- src/main/scala/com/xyzcorp/DefaultImplicits.scala
+++ src/main/scala/com/xyzcorp/DefaultImplicits.scala
@@ -7,4 +7,13 @@ object DefaultImplicits extends App {
   val map = Map(1 -> "One", 2 -> "Two", 3 -> "Three")
   println(map)
   println(map(1))
+
+  //max comes from RichInt
+  println(10.max(3))
+
+  //to is a method in RichInt, this is how a range is created
+  (1 to 10).foreach(println)
+
+  //toHexString is a method in RichInt
+  println(30.toHexString)
 }
