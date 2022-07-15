d884e729a3a8bb907bc831b395e1645e8e97bb5e
omasher <ashermcoa@gmail.com>
2022-07-15 04:13:35 +0300

    Conversion Using a Method

---
+++ src/main/scala/com/xyzcorp/CurrencyApp.scala
@@ -0,0 +1,16 @@
+package com.xyzcorp
+
+import scala.language.implicitConversions
+
+sealed abstract class Currency
+case class Dollar(value: Int) extends Currency
+case class Shilling(value: Int) extends Currency
+case class Euro(value: Int) extends Currency
+
+object CurrencyApp extends App{
+  implicit def int2Dollar(i: Int): Dollar = Dollar(i)
+
+  def combine(x: Dollar, y: Dollar): Dollar = Dollar(x.value + y.value)
+
+  println(combine(100, 100))
+}
