29b53caa24df7098c71da9250233c70e529e8b32
omasher <ashermcoa@gmail.com>
2022-07-15 04:19:33 +0300

    Conversion Using a Function

--- src/main/scala/com/xyzcorp/CurrencyApp.scala
+++ src/main/scala/com/xyzcorp/CurrencyApp.scala
@@ -8,7 +8,9 @@ case class Shilling(value: Int) extends Currency
 case class Euro(value: Int) extends Currency
 
 object CurrencyApp extends App{
-  implicit def int2Dollar(i: Int): Dollar = Dollar(i)
+//  implicit def int2Dollar(i: Int): Dollar = Dollar(i)
+
+  implicit val int2Dollar: Int => Dollar = (i: Int) => Dollar(i)
 
   def combine(x: Dollar, y: Dollar): Dollar = Dollar(x.value + y.value)
 
